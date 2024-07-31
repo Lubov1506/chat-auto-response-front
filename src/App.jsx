835057473965ca7c9682c169b75c9c0b48b1563a
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-07-31 13:02:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ChatPage />}>
           <Route index element={<DefaultContent />} />
-          <Route path="/chats" element={<DefaultContent />} />
+          <Route path="chats" element={<DefaultContent />} />
           <Route path="chats/:chatId" element={<ChatWindow />} />
         </Route>
       </Routes>
