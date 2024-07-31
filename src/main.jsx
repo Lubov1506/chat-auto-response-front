69fc8321b7fda59583740270d42888c4dfffbad1
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-07-31 12:53:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import App from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "../../client/node_modules/react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+// import { ToastContainer } from "../../client/node_modules/react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-    <ToastContainer />
+    {/* <ToastContainer /> */}
   </BrowserRouter>
 );
